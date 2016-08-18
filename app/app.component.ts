1e924767704b282b5bd09fb37ca52c2c9203faf2
Piotr Moskała <piotr.moskala@gmail.com>
2016-08-18 16:09:33 +0200

    services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
 import {Hero} from './hero';
+import { HeroService } from './hero.service';
+import {OnInit} from '@angular/core';
 
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
-  styleUrls:['app/app.component.css']
+  styleUrls:['app/app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent { 
+export class AppComponent implements OnInit{ 
     title = "The tour of heroes";
-    heroes = HEROES;
+    heroes : Hero[];
     selectedHero: Hero;
+    constructor(private heroService: HeroService) {
+      
+    }
+    ngOnInit(){
+      this.getHerose();
+    }
+
+    getHerose(){
+     this.heroService.getHeroesSlowly().then(resoult => this.heroes = resoult);
+    }
 
     onSelect(hero: Hero){
         this.selectedHero = hero;
     }
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
--- app/hero.service.js
+++ app/hero.service.js
@@ -1,9 +1,18 @@
 "use strict";
 var core_1 = require('@angular/core');
+var mock_heroes_1 = require('./mock-heroes');
 core_1.Injectable();
 var HeroService = (function () {
     function HeroService() {
     }
+    HeroService.prototype.getHeroes = function () {
+        return Promise.resolve(mock_heroes_1.HEROES);
+    };
+    HeroService.prototype.getHeroesSlowly = function () {
+        return new Promise(function (resolve) {
+            return setTimeout(function () { return resolve(mock_heroes_1.HEROES); }, 2000);
+        });
+    };
     return HeroService;
 }());
 exports.HeroService = HeroService;
