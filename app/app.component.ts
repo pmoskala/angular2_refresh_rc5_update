50e6bc889dfb61d04744794610b68bb24a0a6445
Piotr Moskała <piotr.moskala@gmail.com>
2016-08-18 12:42:44 +0200

    basic binding

---
+++ app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {Hero} from './hero';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls:['app/app.component.css']
+})
+export class AppComponent { 
+    title = "The tour of heroes";
+    heroes = HEROES;
+    selectedHero: Hero;
+
+    onSelect(hero: Hero){
+        this.selectedHero = hero;
+    }
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
